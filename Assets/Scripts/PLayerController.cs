6ed0f495911793b78b4e39df9eadd214ccad0a9b
Xblade45 <xblade45@gmail.com>
2016-01-30 04:00:26 -0500

    CatchDropController

--- Assets/Scripts/PLayerController.cs
+++ Assets/Scripts/PLayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PLayerController : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class PLayerController : MonoBehaviour
     private Rigidbody rb;
     public float speed;
     Vector3 movement;
+    private int playerScore;
+
+    public Text textbox;
 
 
     // Use this for initialization
@@ -15,27 +19,32 @@ public class PLayerController : MonoBehaviour
     {
         rockGet = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
+        playerScore = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
-
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        
         rb.transform.position += movement * speed;
+        Vector3 temp = rb.position;
+        temp.x = Mathf.Clamp(rb.position.x, -20.0f, 20.0f);
+        rb.position = temp;
 
         rb.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -moveHorizontal * 10 + 1);
+
+        textbox.text = "Score: " + playerScore;
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "allo")
         {
-            Debug.Log("hit");
             rockGet.Play();
             Destroy(other.gameObject);
+            playerScore += 25;
         }
     }
 }
