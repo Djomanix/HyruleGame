529438bec5d9279de7c57302b59ef6983042fe39
Xblade45 <xblade45@gmail.com>
2016-01-30 00:53:55 -0500

    new scene try1

---
+++ Assets/Scripts/PLayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLayerController : MonoBehaviour {
+
+    private AudioSource rockGet = new AudioSource();
+    private Rigidbody rb;
+    public float speed;
+    Vector3 movement;
+
+
+    // Use this for initialization
+    void Start () {
+        rockGet = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        rb.transform.position += movement * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "allo")
+        {
+            Debug.Log("hit");
+            rockGet.Play();
+            Destroy(other.gameObject);
+        }
+    }
+
+}
