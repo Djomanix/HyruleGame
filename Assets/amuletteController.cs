27d231476f0638d8f4d6183db345de2b476e98d4
Xblade45 <xblade45@gmail.com>
2016-01-31 06:13:24 -0500

    Objets terminés

---
+++ Assets/amuletteController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class amuletteController : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float amplitude;
+    public float xPos;
+    public float yOffset;
+
+    Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+        tempPosition = new Vector3(transform.position.x, transform.position.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        tempPosition.x = xPos;
+
+        tempPosition.y = (Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude) + yOffset;
+        
+        transform.position = tempPosition;
+    }
+}
