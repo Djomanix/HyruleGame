6991b31a330fc6ec8cc81fb8eda0957cec3145b1
UnholyDev <ry_wilmink@hotmail.com>
2016-01-31 11:23:28 -0500

    Ritual Stuff again

---
+++ Assets/Scripts/Ritual Scripts/HitScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//print (tag);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Debug.Log ("Hit " + tag);
+
+		if(Input.GetKeyDown(KeyCode.UpArrow) && tag == "up")
+		{
+			//print ("Up Get");
+			Destroy(other.gameObject);
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow) && tag == "down")
+		{
+			//print ("Down get");
+			Destroy(other.gameObject);
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow) && tag == "left")
+		{
+			//print ("Left get");
+			Destroy(other.gameObject);
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow) && tag == "right")
+		{
+			//print ("Right get");
+			Destroy(other.gameObject);
+		}
+	}
+}
