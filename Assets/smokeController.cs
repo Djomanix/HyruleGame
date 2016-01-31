cdcf0b6271ec402bc3da9c443aa977cac4292ac4
Xblade45 <xblade45@gmail.com>
2016-01-31 01:47:10 -0500

    g

--- Assets/smokeController.cs
+++ Assets/smokeController.cs
@@ -3,13 +3,29 @@ using System.Collections;
 
 public class smokeController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    Animator anim;
+
+    private float moveHorizontal;
+    private float moveVertical;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        moveHorizontal = 0.0f;
+        moveVertical = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        if (moveHorizontal != 0 || moveVertical != 0)
+            anim.SetBool("smoking", true);
+        else
+        {
+            anim.SetBool("smoking", false);
+        }
+            
+    }
 }
