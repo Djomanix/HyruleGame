b9275ec3eb0ac1c4fe24a7cd0cc7ab543d207480
UnholyDev <ry_wilmink@hotmail.com>
2016-01-31 03:45:41 -0500

    stuff and things

--- Assets/Scripts/PlayerOverworldController.cs
+++ Assets/Scripts/PlayerOverworldController.cs
@@ -51,9 +51,8 @@ public class PlayerOverworldController : MonoBehaviour {
 		if(horizontal != 0 || vertical != 0)
 		{
 			anim.SetBool("IsWalking", true);
-
 			anim.SetBool("IsDancing", false);
-		}
+        }
 		else
 		{
 			anim.SetBool("IsWalking", false);
